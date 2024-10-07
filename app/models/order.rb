d5950ef8d941571e209c9a77ea1bfcfb42e886da
takanosatomi <ricebread778899@gmail.com>
2024-10-07 12:22:36 +0900

    update_total_quantityにlock

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ApplicationRecord
 
   def update_total_quantity
     self.ordered_lists.each do |line_item|
-      item = Item.find_by(id: line_item.item_id)
+      item = Item.lock.find_by(id: line_item.item_id)
       item.total_quantity += line_item.quantity
       item.save!
     end
