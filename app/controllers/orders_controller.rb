83608b562e561a2b73fa2b84603f2e9bad3ae0ce
takanosatomi <ricebread778899@gmail.com>
2024-10-08 23:49:42 +0900

    lockを一箇所に

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,12 +11,12 @@ class OrdersController < ApplicationController
 
   def create
     ActiveRecord::Base.transaction do
-      @order = current_user.orders.lock.find_by(id: params[:order_id])
+      @order = current_user.orders.find(params[:order_id])
     
-      if @order.nil?
-        @order = current_user.orders.build(order_params)
-      @order.save
-      end
+      # if @order.nil?
+      #   @order = current_user.orders.build(order_params)
+        @order.save
+      # end
 
       # update_total_quantity メソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
       @order.update_total_quantity
